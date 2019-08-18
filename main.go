d629737a2c2cafc615aec4aad9f3ac0d514cd973
Kenneth Buchanan <kbuchanan@Kenneths-MBP-2.fios-router.home>
2019-08-18 16:11:49 -0400

    updates

--- main.go
+++ main.go
@@ -1,36 +1,101 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 	"net/http"
-	"strings"
+	"strconv"
 )
 
+//func sayhelloName(w http.ResponseWriter, r *http.Request) {
+//	r.ParseForm()  // parse arguments, you have to call this by yourself
+//	fmt.Println(r.Form)  // print form information in server side
+//	fmt.Println("path", r.URL.Path)
+//	fmt.Println("scheme", r.URL.Scheme)
+//	fmt.Println(r.Form["url_long"])
+//	for k, v := range r.Form {
+//		fmt.Println("key:", k)
+//		fmt.Println("val:", strings.Join(v, ""))
+//	}
+//	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
+//}
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()  // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form)  // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
+var jokes = []Joke{
+	Joke{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+	Joke{2, 0, "What do you call a fake noodle? An Impasta."},
+	Joke{3, 0, "How many apples grow on a tree? All of them."},
+	Joke{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+	Joke{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+	Joke{6, 0, "Why did the coffee file a police report? It got mugged."},
+	Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
 }
 
 
+type Joke struct {
+	ID int `json:"id" binding:"required"`
+	Likes int `json:"likes"`
+	Joke string `json:"joke" binding:"required"`
+}
+
+// JokeHandler retrieves a list of available jokes
+func JokeHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, jokes)
+}
+
+func LikeJoke(c *gin.Context) {
+	// confirm Joke ID sent is valid
+	// remember to import the `strconv` package
+	if jokeid, err := strconv.Atoi(c.Param("jokeID")); err == nil {
+		// find joke, and increment likes
+		for i := 0; i < len(jokes); i++ {
+			if jokes[i].ID == jokeid {
+				jokes[i].Likes += 1
+			}
+		}
+
+		// return a pointer to the updated jokes list
+		c.JSON(http.StatusOK, &jokes)
+	} else {
+		// Joke ID is invalid
+		c.AbortWithStatus(http.StatusNotFound)
+	}
+}
+
 func main() {
 
-	//gospotify.GetGenreCounts()
-	http.HandleFunc("/", sayhelloName) // set router
-	err := http.ListenAndServe(":9090", nil) // set listen port
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	////gospotify.GetGenreCounts()
+	//http.HandleFunc("/", sayhelloName) // set router
+	//err := http.ListenAndServe(":9090", nil) // set listen port
+	//if err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	//}
+
+
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend views files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H {
+				"message": "pong",
+			})
+		})
 	}
 
+	// Our API will consit of just two routes
+	// /jokes - which will retrieve a list of jokes a user can see
+	// /jokes/like/:jokeID - which will capture likes sent to a particular joke
+	api.GET("/jokes", JokeHandler)
+	api.POST("/jokes/like/:jokeID", LikeJoke)
+
+	// Start and run the server
+	router.Run(":3000")
 
 
 
